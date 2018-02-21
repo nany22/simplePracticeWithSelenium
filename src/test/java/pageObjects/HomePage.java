495b86ad312dbd0c20f96cc66d92319eb79d80d2
Daniela Ojeda <danielaojeda22@gmail.com>
2018-02-21 15:42:41 -0300

    adding documentation

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -11,8 +11,14 @@ import sun.font.TrueTypeFont;
 
 import java.util.List;
 
-/*This page class will go to the home page after login. Pages are the only one using Selenium driver directly*/
-/*Pages will have attributes and methods are used in the current page*/
+/**
+ * This page class will go to the home page after login. Pages are the only one using Selenium driver directly
+ * Pages will have attributes and methods are used in the current page
+ *
+ * <b>inicioTabTextElement</b> is the element that represents Inicio Tab after log in
+ * <b>settingsElement</b> is the element that represents Settings link in the dropDown menu after log in
+ * <b>dropDownElements</b> is a list of elements. It represents the list of all elements in the dropDown menu
+ */
 
 public class HomePage extends PageObject{
     @FindBy(css = "div._5k")
@@ -24,27 +30,36 @@ public class HomePage extends PageObject{
     @FindBy(css = "div._0[role=listitem]")
     List<WebElement> dropDownElements;
 
-
-    /*Constructor: call tu constructor in the super class*/
+   /**
+    * Constructor: call to constructor in the super class {@link PageObject}
+    * @param driver will be sent from test
+    */
     public HomePage(WebDriver driver){
         super(driver);
     }
 
-    /*Obtain the text of Inicio tab webElement, at top of the page*/
+    /**
+     * Obtain the text of "Inicio" tab webElement, at top of the page
+     * @return the text of Inicio tab element
+     */
     public String getInicioTab (){
         return (this.inicioTabTextElemen.getText());
     }
 
-    /*Click on the settings dropdown menu*/
+    /**
+     * Click on the settings dropdown menu link
+     */
     private void clickOnSettings (){
         this.settingsElement.click();
     }
 
-    /*Logs out*/
+    /**
+     * Logs out
+     * We depend on list order because pinterest has for dropdown menu,5 elements with only "class" as value
+     * and they are the same.Logout is the 5th element
+     */
     public void clickOnLogOut (){
         this.clickOnSettings();
-        /* We depend on list order because pintrest has for dropdown menu,5 elements with only class as value and they are the same.Logout is the 5th element*/
         dropDownElements.get(this.dropDownElements.size() - 1).findElement(By.cssSelector("div._0._3j._2n")).click();
     }
-
 }
--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,13 +1,18 @@
 package pageObjects;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import testObjects.HomeTest;
 
-/*This page class will go to the login page only, without log in. Pages are the only one using Selenium driver directly*/
-/*Pages will have attributes and methods are used in the current page*/
+/**
+ * This page class will go to the login page only, without log in.
+ * Pages are the only one using Selenium driver directly
+ * Pages will have attributes and methods are used in the current page
+ *
+ * <b>emailAddressElement</b> represents the input text field for email
+ * <b>passwordElement</b> represents the input text field for password
+ * <b>btnElement</b> represents the submit button when log in
+ */
 
 public class LoginPage extends PageObject {
         @FindBy(css = "input#email")
@@ -22,34 +27,53 @@ public class LoginPage extends PageObject {
         @FindBy(css ="button.lightGrey")
         WebElement loginButtonTextElement;
 
-    /*Constructor: call tu constructor in the super class*/
+    /**
+     * Constructor: call to constructor in the super class {@link PageObject}
+     * @param driver will be sent from test
+    */
     public LoginPage(WebDriver driver){
         super(driver);
     }
 
+    /**
+     * Set email sent as parameter in the email attribute
+     */
     private void setEmailAddress (String emailAddress){
         this.emailAddressElement.sendKeys(emailAddress);
     }
 
+    /**
+     * Set password sent as parameter in the password attribute
+     */
     private void setPassword (String password){
         this.passwordElement.sendKeys(password);
     }
 
+    /**
+     * Click the Submit button after log in
+     */
     private void clickButton (){
         this.btnElement.click();
-
     }
 
-    /*Log in to Pinterest*/
+    /**
+     * Log in to Pinterest using parameters sent
+     * @param emailAddress will be sent from {@link testObjects.LoginTest}
+     * @param password will be sent from {@link testObjects.LoginTest}
+     * @return HomePage class. As best practices when you navigate,you should return the page object for the next page
+     */
     public HomePage loginToPage(String emailAddress, String password){
         setEmailAddress(emailAddress);
         setPassword(password);
         clickButton();
-        /*As best practices when you navigate,you should return the page object for the next page*/
         return (new HomePage(driver));
     }
 
-    /*obtain the text of Log in button (in the right side at top of the page) when you access to the Pinterest url*/
+    /**
+     * obtain the text of "Log in" button (in the right side at top of the page)
+     * when you access to the Pinterest url
+     * @return  the text of log in button
+     */
     public String getLoginButton (){
        return (this.loginButtonTextElement.getText());
     }
